b17a25031a7e84b080da2fd68b0f7e78b97444fc
arsh786 <amzehra@gmail.com>
2017-11-24 23:13:20 -0500

    attempt at adding the slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
+// // Creates a bootstrap-slider element
+
+// console.log("hello world");
+
+$("#yearSlider").slider({
+    tooltip: 'always',
+    tooltip_position:'bottom',
+    min: 1995,
+    max: 2016,
+    step: 1,
+    range: true,
+});
+
+$("#yearSlider").on('change', function(event){
+    // Update the chart on the new value
+    updateYear(event.value.newValue);
+});
+
 var __data__;
 
 var svg = d3.select("svg#chart-area");
+
 
 d3.csv("./data/aircraft_incidents.csv", function(row) {
     row["Total_Fatal_Injuries"] = +row["Total_Fatal_Injuries"];
@@ -15,4 +34,10 @@ d3.csv("./data/aircraft_incidents.csv", function(row) {
     }
     __data__ = data;
     console.log(__data__);
+
+
 });
+
+function updateYear(year) {
+
+}
