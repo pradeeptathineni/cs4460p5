f46bfd3216955e04bcf5d303f58ea043a9b11aa5
Deep <dthineni@gmail.com>
2017-11-29 03:48:00 -0500

    airport data, map data files, incident dots

--- js/main.js
+++ js/main.js
@@ -3,40 +3,103 @@
 // console.log("hello world");
 
 $("#yearSlider").slider({
-    tooltip: 'always',
-    tooltip_position:'bottom',
-    min: 1995,
-    max: 2016,
-    step: 1,
-    range: true,
+    tooltip: "always",
+    tooltip_position: "bottom"
 });
 
-$("#yearSlider").on('change', function(event){
+$("#yearSlider").on("change", function(event){
     // Update the chart on the new value
-    updateYear(event.value.newValue);
+    // updateYear(event.value.newValue);
+    console.log("hi");
 });
-
-var __data__;
 
 var svg = d3.select("svg#chart-area");
+var svgWidth = svg.attr("width");
+var svgHeight = svg.attr("height");
 
+var margin = {top: 0, right: 0, bottom: 0, left: 0};
+var geoWidth = svgWidth - margin.left - margin.right;
+var geoHeight = svgHeight - margin.top - margin.bottom;
 
-d3.csv("./data/aircraft_incidents.csv", function(row) {
-    row["Total_Fatal_Injuries"] = +row["Total_Fatal_Injuries"];
-    row["Total_Serious_Injuries"] = +row["Total_Serious_Injuries"];
-    row["Total_Uninjured"] = +row["Total_Uninjured"];
-    return row;
-}, function(error, data) {
+var fullScale = svgWidth / 2 / Math.PI;
+
+d3.queue()
+    .defer(d3.json, "./data/countries.topojson")
+    .defer(d3.csv, "./data/airport_codes.csv", function(row) {
+        row["Coords"] = [+row["Longitude"], +row["Latitude"]];
+        return row;
+    }).defer(d3.csv, "./data/aircraft_incidents.csv", function(row) {
+        row["Latitude"] = +row["Latitude"];
+        row["Longitude"] = +row["Longitude"];
+        row["Total_Fatal_Injuries"] = +row["Total_Fatal_Injuries"];
+        row["Total_Serious_Injuries"] = +row["Total_Serious_Injuries"];
+        row["Total_Uninjured"] = +row["Total_Uninjured"];
+        return row;
+    }).await(ready);
+
+function ready(error, mapData, portData, planeData) {
     if (error) {
-        console.error("Error while loading ./data/asia_urbanization.csv data.");
+        console.error("Error while queuing data.");
         console.error(error);
         return;
     }
-    __data__ = data;
-    console.log(__data__);
 
+    var airports = {};
 
-});
+    portData.forEach(function(d) {
+        airports[d["Airport_Code"]] = d["Coords"];
+    });
+
+    var countries = topojson.feature(mapData, mapData.objects.countries).features;
+
+    var projection = d3.geoMercator()
+        .translate([svgWidth / 2, svgHeight / 2])
+        .scale(fullScale);
+
+    var path = d3.geoPath()
+        .projection(projection);
+
+    svg.selectAll(".country")
+        .data(countries)
+        .enter().append("path")
+        .attr("class", "country")
+        .attr("d", path);
+
+    svg.selectAll(".incident-dot")
+        .data(planeData)
+        .enter().append("circle")
+        .attr("class", "incident-dot")
+        .attr("r", 2)
+        .attr("cx", function(d) {
+            if (d.Longitude && d.Latitude) {
+                coords = projection([d.Longitude, d.Latitude]);
+            } else if (d.Airport_Code) {
+                try {
+                    var port = airports[d.Airport_Code];
+                    coords = projection([port[0], port[1]]);
+                } catch (e) {
+                    coords = projection([d.Longitude, d.Latitude]);
+                }
+            } else {
+                coords = projection([d.Longitude, d.Latitude]);
+            }
+            return coords[0];
+        }).attr("cy", function(d) {
+            if (d.Longitude && d.Latitude) {
+                coords = projection([d.Longitude, d.Latitude]);
+            } else if (d.Airport_Code) {
+                try {
+                    var port = airports[d.Airport_Code];
+                    coords = projection([port[0], port[1]]);
+                } catch (e) {
+                    coords = projection([d.Longitude, d.Latitude]);
+                }
+            } else {
+                coords = projection([d.Longitude, d.Latitude]);
+            }
+            return coords[1];
+        });
+}
 
 function updateYear(year) {
 
