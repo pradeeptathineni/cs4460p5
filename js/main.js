b988f68016cd396135978c19d5681e4bf7dbac04
Deep <dthineni@gmail.com>
2017-11-29 20:02:03 -0500

    details area is packed, we can choose whats important later

--- js/main.js
+++ js/main.js
@@ -13,10 +13,10 @@ $("#yearSlider").on("change", function(event){
     console.log("hi");
 });
 
-var svg = d3.select("svg#chart-area");
-var details = d3.select("svg#details");
-var svgWidth = svg.attr("width");
-var svgHeight = svg.attr("height");
+var geo = d3.select("svg#geo-area");
+var details = d3.select("svg#details-area");
+var svgWidth = geo.attr("width");
+var svgHeight = geo.attr("height");
 
 var margin = {top: 0, right: 0, bottom: 0, left: 0};
 var geoWidth = svgWidth - margin.left - margin.right;
@@ -60,17 +60,17 @@ function ready(error, mapData, portData, planeData) {
     var path = d3.geoPath()
         .projection(projection);
 
-    svg.selectAll(".country")
+    var countries = geo.selectAll(".country")
         .data(countries)
         .enter().append("path")
         .attr("class", "country")
         .attr("d", path);
 
-    svg.selectAll(".incident-dot")
+    var dots = geo.selectAll(".incident-dot")
         .data(planeData)
         .enter().append("circle")
         .attr("class", "incident-dot")
-        .attr("r", 2)
+        .attr("r", 3)
         .attr("cx", function(d) {
             if (d.Longitude && d.Latitude) {
                 coords = projection([d.Longitude, d.Latitude]);
@@ -100,29 +100,117 @@ function ready(error, mapData, portData, planeData) {
             }
             return coords[1];
         })
-        .on("mouseover", handleMouseOver)
-        .on("mouseout", handleMouseOut);
-
-
+        .on("mouseover", incidentMouseover)
+        .on("mouseout", incidentMouseout);
 
 }
 
-function handleMouseOver(d, i) {
-    details.attr("id", "#t" + d.Accident_Number).append("rect").attr("x", 0).attr("y", 0).attr("width", 300)
-    .attr("height", 500).style("fill", "#E9E9E9").attr("rx", 15).attr("ry", 15);
+var interval;
+function incidentMouseover(d, i) {
+    clearInterval(interval);
+    $(".incident-dot").attr("fill", "black");
+    var _this = $(this);
+    _this.attr("fill", "red");
+    // interval = window.setInterval(function() {
+    //     if (_this.attr("fill") == "red") {
+    //         _this.attr("fill", "black");
+    //     } else {
+    //         _this.attr("fill", "red");
+    //     }
+    // }, 1000);
+    details.selectAll("text").remove();
+    var y = 25;
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", y)
+        .text("Accident Number: " + d.Accident_Number);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 2*y)
+        .text("Event Date: " + d.Event_Date);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 3*y)
+        .text("Location: " + d.Location);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 4*y)
+        .text("Country: " + d.Country);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 5*y)
+        .text("Latitude: " + d.Latitude);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 6*y)
+        .text("Longitude: " + d.Longitude);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 7*y)
+        .text("Airport Code: " + d.Airport_Code);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 8*y)
+        .text("Airport Name: " + d.Airport_Name);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 9*y)
+        .attr("fill", "red")
+        .text("Injury Severity: " + d.Injury_Severity);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 10*y)
+        .attr("fill", "red")
+        .text("Aircraft Damage: " + d.Aircraft_Damage);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 11*y)
+        .attr("fill", "red")
+        .text("Make: " + d.Make);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 12*y)
+        .attr("fill", "red")
+        .text("Model: " + d.Model);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 13*y)
+        .attr("fill", "red")
+        .text("Air_Carrier: " + d.Air_Carrier);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 14*y)
+        .attr("fill", "red")
+        .text("Total Fatal Injuries: " + d.Total_Fatal_Injuries);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 15*y)
+        .attr("fill", "red")
+        .text("Total Serious Injuries: " + d.Total_Serious_Injuries);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 16*y)
+        .attr("fill", "red")
+        .text("Total Uninjured: " + d.Total_Uninjured);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 17*y)
+        .text("Schedule: " + d.Schedule);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 18*y)
+        .text("Weather Condition: " + d.Weather_Condition);
+    details.append("text")
+        .attr("x", 15)
+        .attr("y", 19*y)
+        .text("Broad Phase of Flight: " + d.Broad_Phase_of_Flight);
+}
 
-    var y = 15;
-    details.append("text").attr("x", 15).attr("y", y).text("Accident Num: " + d.Accident_Number);
-    details.append("text").attr("x", 15).attr("y", 2*y).text("Country: " + d.Country);
-    details.append("text").attr("x", 15).attr("y", 3*y).text("Location: " + d.Location);
-    details.append("text").attr("x", 15).attr("y", 4*y).text("Date: " + d.date);
-    console.log(d);
-    //note Currently it does not remove the element when you move the mouse away     
+function incidentMouseout(d, i) {
+
 }
-function handleMouseOut(d, i) {
-    details.selectAll("*").remove();
-    // d3.select("#t"+d.Accident_Number).remove();
-}
+
+
 function updateYear(year) {
 
 }
